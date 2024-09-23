d2a793588d2bc1ae832e350ce4409022b89fa54e
zakaria loulida <zakarialoulida92@gmail.com>
2024-09-23 11:03:30 +0100

    ajout de controller pour client

--- src/main/com/baticuisine/controller/ClientController.java
+++ src/main/com/baticuisine/controller/ClientController.java
@@ -1,4 +1,40 @@
 package main.com.baticuisine.controller;
 
+
+import main.com.baticuisine.model.Client;
+import main.com.baticuisine.service.ClientService;
+
+import java.util.List;
+import java.util.UUID;
+
 public class ClientController {
+
+    private final ClientService clientService;
+
+    public ClientController() {
+        this.clientService = new ClientService();
+    }
+
+    public void createClient(Client client) {
+        clientService.createClient(client);
+    }
+
+    public Client getClientById(UUID id) {
+        return clientService.getClientById(id);
+    }
+    public Client getClientByName(String name) {
+        return clientService.getClientByName(name);
+    }
+
+    public List<Client> getAllClients() {
+        return clientService.getAllClients();
+    }
+
+    public void updateClient(Client client) {
+        clientService.updateClient(client);
+    }
+
+    public void deleteClient(UUID id) {
+        clientService.deleteClient(id);
+    }
 }
